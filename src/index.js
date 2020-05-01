ea903bcce5cf7ecbd0317a894d6fb2affb00b595
Sarah Strange <strange1120@gmail.com>
2020-04-30 21:56:57 -0400

    adding query and mutation for posts

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+// 1
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
+  }
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
